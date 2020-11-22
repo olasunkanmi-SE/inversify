fd496a83d2f01862bc84f89be35c97d2892732d9
raymond-frontend <raymond.oyinlola@gmail.com>
2020-11-22 16:31:13 +0800

    create test for book service

--- src/repository/book-repo.ts
+++ src/repository/book-repo.ts
@@ -1,4 +1,3 @@
-import { IBook } from "./../domain/book/bookproperties";
 import { IBookRepository } from "./book-repo.interface";
 import { getConnection } from "typeorm";
 import { Book } from "../entities/book.entity";
